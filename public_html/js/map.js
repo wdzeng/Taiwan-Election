df2ed5b09477d1d3bde5ccaf32c74fac9975cf6b
Hyperbola <hyperbola.cs07@gmail.com>
2018-12-16 07:16:33 +0800

    Bind zooming.

--- public_html/js/map.js
+++ public_html/js/map.js
@@ -1,15 +1,15 @@
 "use strict";
 const file = '../res/topojson/2.json';
 
-function drawMaps(dSvgs, zoomable = true, topoData) {
+function drawMaps(dSvgs, topoData) {
     if (!topoData) {
-        d3.json(file).then(topoData => { drawMaps(dSvgs, topoData, zoomable) });
+        d3.json(file).then(topoData => { drawMaps(dSvgs, topoData) });
         return;
     }
-    dSvgs.forEach(dSvg => drawMap(dSvg, topoData, zoomable));
+    dSvgs.forEach(dSvg => drawMap(dSvg, topoData));
 }
 
-function drawMap(dSvg, zoomable = true, topoData) {
+function drawMap(dSvg, topoData) {
 
     let sc = scale(dSvg);
     if (!topoData) {
@@ -17,22 +17,24 @@ function drawMap(dSvg, zoomable = true, topoData) {
         return;
     }
 
-    // Get geojson data (features collection)
-    // let geoData = topojson.feature(topoData, topoData.objects.villages);
-    let taiwan = topojson.merge(topoData, topoData.objects.villages.geometries);
-    let villages = topojson.feature(topoData, topoData.objects.villages);
-
     // Make projection
+    let taiwan = topojson.merge(topoData, topoData.objects.villages.geometries);
     let proj = d3.geoMercator().fitExtent([[sc * 20, sc * -580], [sc * 930, sc * 1240]], taiwan);
     let renderer = d3.geoPath().projection(proj);
 
     // Draw on map
-    let dC = dSvg.append('g');
+    let dC = dSvg.append('g').attr('class', 'map-root');
+
+    // Draw taiwan
     dC.append('g')
         .attr('class', 'taiwan')
         .append('path')
         .datum(taiwan)
         .attr('d', renderer);
+    taiwan = null;
+
+    // Draw villages
+    let villages = topojson.feature(topoData, topoData.objects.villages);
     dC.append('g')
         .attr('class', 'village')
         .selectAll('path')
@@ -41,16 +43,21 @@ function drawMap(dSvg, zoomable = true, topoData) {
         .append('path')
         .attr('d', renderer)
         .attr('v', d => d.properties.v);
+    villages = null;
 
-    if (!zoomable) return;
+}
+
+function bindZoom(dSvgs) {
+    let sc = scale(dSvgs);
     let zoom = d3.zoom()
-        .on("zoom", () => {
-            dC.attr("transform", d3.event.transform);
-        })
+        .on("zoom", () => dSvgs.selectAll('.map-root').attr("transform", d3.event.transform))
         .scaleExtent([1, 100])
         .translateExtent([[0, 0], [sc * 600, sc * 800]])
-
-    dSvg.call(zoom);
+    dSvgs.call(zoom);
+    dSvgs.nodes().forEach(n => {
+        n.onwheel = e => e.preventDefault();
+        n.onmousewheel = e => e.preventDefault();
+    })
 }
 
 function scale(dE) {
