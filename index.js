79e5cdc24e4926bc50e919571d25d95aaa1d2a84
Hyperbola <hyperbola.cs07@gmail.com>
2018-12-17 07:40:23 +0800

    Rearrange.

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ let exp = require('express');
 let app = exp();
 
 app.use(exp.static('public_html'));
+app.get('/q', (req, res) => {
+    console.log(JSON.stringify(req.query));
+});
 app.get('/', (req, res) => {
     res.send('Hello world!');
 });
 
-app.listen(3000, () => console.log("Server starts."))
+app.listen(3000, () => console.log("Server starts."));
